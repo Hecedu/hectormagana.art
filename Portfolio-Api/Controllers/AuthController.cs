0cea48f865a181e4758ad31c161336c62caea977
Hecedu <hector.maganahdez@gmail.com>
2022-03-27 01:49:50 -0600

    Actions and API fix

--- Portfolio-Api/Controllers/AuthController.cs
+++ Portfolio-Api/Controllers/AuthController.cs
@@ -5,28 +5,29 @@ using Portfolio_Api.Models;
 using Portfolio_Api.Services;
 using static checkers_api.Services.AuthService;
 
-namespace checkers_api.Controllers;
-
-[ApiController]
-[Route("/api/[controller]")]
-public class AuthController : ControllerBase
+namespace checkers_api.Controllers
 {
-    private readonly ILogger<AuthController> logger;
-    private readonly IAuthService authService;
-
-    public AuthController(ILogger<AuthController> logger, IAuthService authService, IConfiguration configuration)
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
     {
-        this.logger = logger;
-        this.authService = authService;
-    }
-    [HttpPost("authenticate")]
-    public IActionResult Authenticate([FromBody] AuthenticateRequest data)
-    {
-        GoogleJsonWebSignature.ValidationSettings settings = new GoogleJsonWebSignature.ValidationSettings();
-        settings.Audience = new List<string>() { "216463964469-1uop6pd4rfv1e8e2i2ajk585hl51g8o2.apps.googleusercontent.com" };
+        private readonly ILogger<AuthController> logger;
+        private readonly IAuthService authService;
 
-        GoogleJsonWebSignature.Payload payload = GoogleJsonWebSignature.ValidateAsync(data.IdToken, settings).Result;
-        var authtoken = authService.CreateToken(payload.Email);
-        return Ok(new { AuthToken = authService.CreateToken(payload.Email) });
+        public AuthController(ILogger<AuthController> logger, IAuthService authService, IConfiguration configuration)
+        {
+            this.logger = logger;
+            this.authService = authService;
+        }
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] AuthenticateRequest data)
+        {
+            GoogleJsonWebSignature.ValidationSettings settings = new GoogleJsonWebSignature.ValidationSettings();
+            settings.Audience = new List<string>() { "216463964469-1uop6pd4rfv1e8e2i2ajk585hl51g8o2.apps.googleusercontent.com" };
+
+            GoogleJsonWebSignature.Payload payload = GoogleJsonWebSignature.ValidateAsync(data.IdToken, settings).Result;
+            var authtoken = authService.CreateToken(payload.Email);
+            return Ok(new { AuthToken = authService.CreateToken(payload.Email) });
+        }
     }
 }
--- Portfolio-Client/src/Services/auth-service.ts
+++ Portfolio-Client/src/Services/auth-service.ts
@@ -1,7 +1,4 @@
 import axios from "axios";
-import User from "../Models/user";
-
-const authUrl = "/api/auth";
 
 const authenticateUser = async (token: string): Promise<string> => {
   const res = (await axios.post('/api/Auth/authenticate', {idToken: token})).data.authToken;
