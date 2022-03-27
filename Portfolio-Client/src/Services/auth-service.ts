e15b0d127bceac786bbd5982c5ac8cf1099575e4
Hecedu <hector.maganahdez@gmail.com>
2022-03-27 02:19:53 -0600

    typo

--- Portfolio-Client/src/Services/auth-service.ts
+++ Portfolio-Client/src/Services/auth-service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const authenticateUser = async (token: string): Promise<string> => {
   const res = (await axios.post('/api/Auth/authenticate', {idToken: token})).data.authToken;
-  return res.data;
+  return res;
 };
 
 const authService = {
